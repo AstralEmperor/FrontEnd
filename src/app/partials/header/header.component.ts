03c1508f270b8354b9cdc5f0f2af9bb46be5b580
AstralEmperor <96700405+AstralEmperor@users.noreply.github.com>
2023-09-12 21:15:45 +0200

    display = '';

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent {
     window.location.hash = section
   }
   
-  display = 'flex';
+  display = '';
   
   toggleMenu(event: any):string{
     if(this.display === 'none'){
